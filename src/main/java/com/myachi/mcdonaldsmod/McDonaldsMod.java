76a3f8b51b49d9f3d43a5fa585250296a23e0fdd
Myachion <1612959049@qq.com>
2026-02-19 17:55:20 +0800

    Add a test item

--- src/main/java/com/myachi/mcdonaldsmod/McDonaldsMod.java
+++ src/main/java/com/myachi/mcdonaldsmod/McDonaldsMod.java
@@ -18,7 +18,7 @@ public class McDonaldsMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-        TestItems.registerTestItems();
+        TestItems.initializeMod();
 		LOGGER.info("Hello Fabric world!");
 
 	}
--- src/main/java/com/myachi/mcdonaldsmod/TestItems.java
+++ src/main/java/com/myachi/mcdonaldsmod/TestItems.java
@@ -1,22 +1,34 @@
 package com.myachi.mcdonaldsmod;
 
 import net.minecraft.item.Item;
+
+
+import net.minecraft.item.Items;
 import net.minecraft.item.equipment.trim.ArmorTrimMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.resource.*;
+
+import java.util.function.Function;
 
 public class TestItems {
-    public static final Item STEEL_INGOT = registerItems("steel_ingot",new Item(new Item.Settings()));
 
-    private static Item registerItems(String id, Item item) {
-        //return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(McDonaldsMod.MOD_ID,id)),item);
-        return Registry.register(Registries.ITEM,Identifier.of(McDonaldsMod.MOD_ID,id),item);
+
+    public static final Item STEEL_INGOT = register("steel_ingot",Item::new,new Item.Settings());
+
+    private TestItems() {}
+
+    public static Item register(String path, Function<Item.Settings,Item> factory,Item.Settings settings) {
+        final RegistryKey<Item> registerKey = RegistryKey.of(RegistryKeys.ITEM,Identifier.of(McDonaldsMod.MOD_ID,path));
+        return Items.register(registerKey,factory,settings);
     }
-    public static void registerTestItems(){
+
+    public static void initializeMod() {
         McDonaldsMod.LOGGER.info("Register TestItems!");
     }
+
 
 }
