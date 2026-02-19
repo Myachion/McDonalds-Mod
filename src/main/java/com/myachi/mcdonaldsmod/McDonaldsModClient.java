4113dadd7aa0753036cbd2894ac2005c576721fe
Myachion <1612959049@qq.com>
2026-02-19 14:55:56 +0800

    Initial commit

---
+++ src/main/java/com/myachi/mcdonaldsmod/McDonaldsModClient.java
@@ -0,0 +1,11 @@
+package com.myachi.mcdonaldsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McDonaldsModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
